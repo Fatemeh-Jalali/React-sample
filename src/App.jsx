f5ac85c8aedf92b217a988f2c774ed9a1d9009e4
Fatemeh Jalali <fatemehjalalidehkordi@gmail.com>
2024-08-11 18:56:49 +0330

    creat characterlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 
+import { characters } from "../data/data";
 import "./App.css";
+import CharacterDetail from "./component/CharacterDetail";
+import CharacterList from "./component/CharacterList";
+import Navbar from "./component/Navbar";
 
 function App(){
   return 
   <div className="app">
     <Navbar />
     <div className="main">
-      
+      <CharacterList characters={characters} />
+      <CharacterDetail />
+
     </div>
   </div>
 
