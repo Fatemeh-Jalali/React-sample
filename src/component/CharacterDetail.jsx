9eb2dc9e1139ac4c68edb5d152f850873cf70f0b
Fatemeh Jalali <fatemehjalalidehkordi@gmail.com>
2024-08-15 14:50:37 +0330

    character datail component

--- src/component/CharacterDetail.jsx
+++ src/component/CharacterDetail.jsx
@@ -1,7 +1,54 @@
-
+import { ArrowUpCircleIcon } from "@heroicons/react/20/solid";
+import { character, episodes }  from "../../data/data";
 function CharacterDetail() {
   return (
-    <div>CharacterDetail</div>
+    <div style={{flex:1}}>
+      <div className="character-detail">
+        <img src={character.image} alt={character.name} className="character-detail__img" />
+        <div className="character-detail__info">
+          <h3 className="name">
+            <span>{character.gender=== "Male" ? "*" : "#"}</span>
+            <span>{character.name}</span>
+          </h3>
+          <div className="info">
+            <span className={`status ${character.status === "Dead" ? "red" :""}`}>
+
+              <span>{character.status}</span>
+              <span>{character.species}</span>
+            </span>
+          </div>
+          <div className="location">
+            <p>last khown location</p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="action">
+            <button className="btn btn--primary">add to Favourite</button>
+          </div>
+        </div>
+      </div>
+      <div className="character-opisodes">
+        <div className="title">
+          <h2>list of episodes:</h2>
+          <button>
+            <ArrowUpCircleIcon className="icon" />
+          </button>
+
+        </div>
+        <ul>
+         
+            {episodes.map((item, index) => (
+            <li key={item.id}>
+              <div>
+               {String(index +1).padStart(2, "0")} - {item.episode} : <strong>{item.name}</strong>
+              </div>
+              <div className="badge badge--secendary">{item.air_date}</div>
+
+            </li>
+            ))
+          }
+        </ul>
+      </div>
+    </div>
   )
 }
 
